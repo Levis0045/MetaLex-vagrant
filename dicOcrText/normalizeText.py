82fd6ac2b7edb84dc8ef2960d1004fc668ac6861
levis0045 <Levis0045@users.noreply.github.com>
2017-06-08 01:11:50 +0200

    modified file extraction

--- dicOcrText/normalizeText.py
+++ dicOcrText/normalizeText.py
@@ -2,168 +2,223 @@
 # coding: utf8
 
 """
-    Implémentation des outils de normalization de l'image.
+    Implémentation des outils de normalization du texte des articles.
  
+    apt-get install python-html5lib
+    apt-get install python-lxml
+    apt-get install python-bs4
+    
     Usage:
- 
-    >>> from DICOparser.dicImage import *
-    >>> dicoNormalize()
+    >>> from MetaLex.dicOcrText import *
+    >>> makeTextWell()
 """
+
+# ----Internal Modules------------------------------------------------------
 
 import MetaLex
 
-# ----------------------------------------------------------
+# ----External Modules------------------------------------------------------
 
-from string import rstrip
-import re, sys
-from collections import Counter
+import re, sys, codecs
+from bs4 import BeautifulSoup
+import warnings
+from array import array
 
-# ----------------------------------------------------------
+# -----Exported Functions-----------------------------------------------------
 
-__all__ = ['makeTextWell', 'readFile', 'correction']
+__all__ = ['makeTextWell', 'fileRule']
 
-# ----------------------------------------------------------
-
+# -----Global Variables-----------------------------------------------------
 
 dicArticles = []
 AllWords = []
 
+# ----------------------------------------------------------
 
-def fileRuleUnpack(file):
-    return false
     
-    
-    
-def makeTextWell(filerule):
-    
-    with open(file, 'r') as f :
-        #tabCategorie = ["adj. ", "n. ", "n. m. ", "n. f. ", "adv.", "prép.", "v.", "n."]
-        article = ""
-        #regextext = re.compile(r"(adj.|n.|m.|n.|f.|adv.|prép.|v.|n.)", re.M)
-        for ligne in f :
-            ligne = str(rstrip(ligne))
-            if re.match(r"\w+", ligne):
-                #print ligne[-1]
-                if not(re.match(r"^[A-Z]", ligne)) : 
-                    if ligne[-1] != "." :
-                        article += ligne
-                        #print "%s " %ligne        
-                    if ligne[-1] == "." :
-                        article +=" "+ligne+" "
-                        article = re.sub(r"(-|—|— |- )",r"", article, re.M)
-                        dicArticles.append(article)
-                        #print "%s\n" %article
-                        article = ""
-    
-    for art in dicArticles :
-        print art, '\n'
-        words = art.split()
-        AllWords.append(words)
+def makeTextWell(file_rules, okCorrect=False):
+    filerule = fileRule(file_rules)
+    data_rules = filerule.fileRuleUnpack()
+    html_files = MetaLex.resultOcrFiles
+    for html in html_files :
+        with open(html, 'r') as h :
+            extractArticle(h, data_rules, okCorrect)
+            
         
-    #return dicArticles
+def extractArticle(html_file, data, okCorrect):
+    soup = BeautifulSoup(html_file, "html5lib")
+    div = soup.find('div', attrs={'class': u'ocr_page'}) 
+    art = 1
+    
+    for div in div.findAll('div', attrs={'class': u'ocr_carea'}) :
+        for para in div.findAll('p', attrs={'class': u'ocr_par'}) :
+            contentOrigin = u''
+            contentCorrection = u''
+            for span in para.stripped_strings:
+                if span[-1] == u'—' or span[-1] == u'-':
+                    span = span[:-1]
+                    AllWords.append(span)
+                    if okCorrect :
+                        spanCorrect = correctWord(span)
+                        contentCorrection += spanCorrect
+                    else :
+                        contentOrigin += span+u' '
+                    #print '*****  '+span + ' : ' + spanCorrect
+                elif wordReplace(span, data[1], test=True) :
+                    span = wordReplace(span, data[1])
+                    if okCorrect :
+                        spanCorrect = correctWord(span)
+                        contentCorrection += spanCorrect+u' '
+                    else :
+                        contentOrigin += span+u' '
+                    #print '*****  '+span + ' : ' + spanCorrect
+                elif caractReplace(span, data[2], test=True):
+                    span = caractReplace(span, data[2])
+                    AllWords.append(span)
+                    if okCorrect :
+                        spanCorrect = correctWord(span)
+                        contentCorrection += spanCorrect+u' '
+                    else:
+                        contentOrigin += span+u' '
+                    #print '*****  '+span + ' : ' + spanCorrect
+                else:
+                    if okCorrect :
+                        AllWords.append(span)
+                        spanCorrect = correctWord(span)
+                        contentCorrection += spanCorrect+u' '
+                    else :
+                        AllWords.append(span)
+                        contentOrigin += span+u' '
+                    #print '*****  '+span + ' : ' + spanCorrect
+            artnum = 'art'+str(art)
+            crtnum = 'correct'+str(art)
+            if len(contentOrigin) >= 5 :
+                article = {artnum:contentOrigin, crtnum:contentCorrection}
+                dicArticles.append(article)
+                art += 1
 
-    """
-    try: 
-        if words[1][-1] == '.' :
-            #print words[1]
-    except :
-        print "*****c'est erreur, pas de panique********"
+    for article in dicArticles :
+        for art in article.items()[0]:
+            print art+'\n'
+
+
+def correctWord (word):
+    correct = MetaLex.wordCorrection()
+    if len(word) > 1 :
+        word = word.strip()
+        if word[-1] in [u'.', u',']:
+            fin = word[-1]
+            if word[0].isupper() :
+                deb = word[0]
+                wordc = word[:-1]
+                goodword = correct.correction(wordc.lower())
+                wordg = deb+goodword[1:]+fin
+                return wordg
+            else : 
+                wordc = word[:-1]
+                goodword = correct.correction(wordc)
+                wordg = goodword+fin
+                return wordg
+        elif word[-1] in [u')']:
+            return word
+        elif word[1] in [u"'", u"’"] :
+            deb = word[:2]
+            wordc = word[2:]
+            goodword = correct.correction(wordc)
+            wordg = deb+goodword[1:]
+            return wordg
+        elif word[0] in [u":"] :
+            deb = word[0]
+            wordc = word[1:]
+            goodword = correct.correction(wordc)
+            wordg = deb+goodword[1:]
+            return wordg
+        else :
+            goodword = correct.correction(word)
+            return goodword
+    else :
+        return word
         
-    print "\n\n" 
-    """
-                           
-def readFile ():
-   
-    with open(file, 'r') as f :
-        for ligne in f :
-            ligne = str(rstrip(ligne))
-            if re.match(r"\w+", ligne):
-                print ligne                       
-
-
-# functions for Word spelling proposed in http://norvig.com/spell-correct.html
-  
-def words(text): 
-    w = text.lower().split()
-    return w
-
-WORDS = Counter(words(open('ressourcesTextuelles/dela-fr.txt').read()))
-
-def P(word, N=sum(WORDS.values())): 
-    "Probability of `word`."
-    return WORDS[word] / N
-
-def correction(word): 
-    "Most probable spelling correction for word."
-    return max(candidates(word), key=P)
-
-def candidates(word): 
-    "Generate possible spelling corrections for word."
-    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
-    print  (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
-
-def known(words): 
-    "The subset of `words` that appear in the dictionary of WORDS."
-    #print set(w for w in words if w in WORDS)
-    return set(w for w in words if w in WORDS)
-
-def edits1(word):
-    "All edits that are one edit away from `word`."
-    letters    = "abcdefghijklmnopqrstuvwxyzéèêîiïàùûâ'"
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
-    #print inserts
-    return set(deletes + transposes + replaces + inserts)
-
-def edits2(word): 
-    "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word) for e2 in edits1(e1))  
-
-
-
-
-
-
-"""
-class wordCorrection :
-    def __init__(self):
-        self.WORDS = Counter(words(open('big.txt').read()))
         
-    def words(self, text): return re.findall(r'\w+', text.lower())
+def wordReplace(word, data, test=False):
+    equiv_words = data
+    if test :
+        if equiv_words.has_key(word) :
+            return True
+    else :
+        if  word in equiv_words.keys() :
+            return equiv_words[word]
     
     
-    def P(self, word, N=sum(WORDS.values())): 
-        "Probability of `word`."
-        return self.WORDS[word] / N
+def caractReplace(word, data, test=False):
+    equiv_caract = data
+    if test :
+        for k in equiv_caract.items()[0]:
+            if word.find(k):
+                return True
+            else :
+                return False
+    else:
+        for ke in equiv_caract.keys() :
+            if word.find(ke):
+                return word.replace(ke, equiv_caract[ke])
+            break
+        
+        
+class fileRule():
     
-    def correction(self, word): 
-        "Most probable spelling correction for word."
-        return max(self.candidates(word), key=self.P)
+    def __init__(self, file_rule):
+        self.file = file_rule
+        
+        
+    def fileRuleUnpack(self):
+        word, caracter, regex = '\W', '\C', '\R'
+        metadata, ruleWords, ruleCaracts, ruleRegex = {}, {}, {}, {}
+        startw, startc, startr = False, False, False
+        
+        if self.verify() :
+            with codecs.open(self.file, 'r', 'utf-8') as rule :
+                for line in rule : 
+                    line = line.strip()
+                    if line.startswith('\MetaLex') : 
+                        names = ('tool', 'project', 'theme', 'lang', 'admin', 'date')
+                        for name, cnt in zip(names, line.split('\\')[1:]) :
+                            metadata[name] = cnt
+                    if line == word : startw, startc, startr = True, False, False
+                    if line == caracter : startw, startc, startr = False, True, False
+                    if line == regex : startw, startc, startr = False, False, True
+                    if startw :
+                        linepart = line.split('/')
+                        if len(linepart) == 3 : ruleWords[linepart[1]] = linepart[2]
+                    if startc :
+                        linepart = line.split('/')
+                        if len(linepart) == 3 : ruleCaracts[linepart[1]] = linepart[2]
+                    if startr :
+                        linepart = line.split('/')
+                        if len(linepart) == 3 : ruleRegex[linepart[1]] = linepart[2]
+        else :
+            warnings.warn("Your file syntax is not correct. Please correct it as recommended")
+                    
+        return metadata, ruleWords, ruleCaracts, ruleRegex 
+        
     
-    def candidates(self, word): DICOparser
-        "Generate possible spelling corrections for word."
-        return (self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word)) or [word])
+    def verify(self):
+        module, synw, sync, synr, synrw, delimiter = (False for x in range(6))
+        fileop = codecs.open(self.file, 'r', 'utf-8').readlines()
+        if '\START' == fileop[0].strip() and '\END' == fileop[-1].strip() : delimiter = True
+        if len(fileop[1].strip().split('\\')) == 7 :
+            for el in fileop[1].strip().split('\\') :
+                if el == 'MetaLex': module = True
+        for lg in fileop:
+            lg = lg.strip()
+            if lg == '\W' : synw = True
+            if lg == '\C' : sync = True
+            if lg == '\R' : synr = True
+            if lg[0] == '/' : 
+                if len(lg.split('/')) == 3 : synrw = True
+        if sync and synw and synr and module and delimiter and synrw :
+            return True
+        else :
+            return False
     
-    def known(self, words): 
-        "The subset of `words` that appear in the dictionary of WORDS."
-        return set(w for w in words if w in self.WORDS)
     
-    def edits1(self, word):
-        "All edits that are one edit away from `word`."
-        letters    = 'abcdefghijklmnopqrstuvwxyz'
-        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-        deletes    = [L + R[1:]               for L, R in splits if R]
-        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-        inserts    = [L + c + R               for L, R in splits for c in letters]
-        return set(deletes + transposes + replaces + inserts)
-    
-    def edits2(self, word): 
-        "All edits that are two edits away from `word`."
-        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))  
-
-"""
-                     
