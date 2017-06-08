9744b1a30f23c21ad7e83ab1396690cd0289c30c
levis0045 <Levis0045@users.noreply.github.com>
2017-06-08 10:45:11 +0200

    ok

--- dicPlugins/__init__.py
+++ dicPlugins/__init__.py
@@ -2,5 +2,8 @@
 # coding: utf8 
 
 import sys, os
-pytesserocr = os.path.dirname(os.path.abspath(__file__))
+pytesserocr = os.path.dirname(os.path.abspath(__file__))+'/pytesseocr'
+resources = pytesserocr = os.path.dirname(os.path.abspath(__file__))+'/resources'
+
 sys.path.append(pytesserocr)
+sys.path.append(resources)
