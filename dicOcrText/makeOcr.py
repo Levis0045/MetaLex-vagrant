11432199b60b3564ec9ffda155ed9738c52f6ce6
levis0045 <Levis0045@users.noreply.github.com>
2017-06-08 10:46:40 +0200

    ok

--- dicOcrText/makeOcr.py
+++ dicOcrText/makeOcr.py
@@ -61,17 +61,18 @@ def imageToText(show=False, save=False, langIn='fra'):
             for i in imagepart :
                 imageconcat +='_'+i 
             imageconcat = imageconcat.split('.')[0]
-            tempname = 'text_ocr'+imageconcat+'.html'
+            tempname = 'text_ocr'+imageconcat+'_'+str(num)+'.html'
             
-            print "\nDébut de la lecture optique de '"+imagefile+"'\n"
-            textocr = api.GetHOCRText(100)
-            print "\nFin de la lecture optique de '"+imagefile+"'\n"
+            print "\n--> Début de la lecture optique de '"+imagefile+"'\n"
+            textocr = api.GetHOCRText(0)
+            print "\n--> Fin de la lecture optique de '"+imagefile+"'\n"
             
             if save:
                 dicProject.createtemp()
                 with codecs.open(tempname, 'w', "utf-8") as wr :
                     wr.write(textocr)
                 message = "'"+ imagefile +"' is Ocrised to > '"+tempname+"' > Saved in dicTemp folder" 
+                print "--> "+message+"\n"
                 MetaLex.dicLog.manageLog.writelog(message) 
             elif show :
                 print "\n\n*********************************************************\n\n"
