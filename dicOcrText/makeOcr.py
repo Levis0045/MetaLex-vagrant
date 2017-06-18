814eaf07fb63ffe22cea1afdb5fce08b0a7e6517
levis0045 <Levis0045@users.noreply.github.com>
2017-06-18 13:53:40 +0200

    correct repetitive saving

--- dicOcrText/makeOcr.py
+++ dicOcrText/makeOcr.py
@@ -24,7 +24,7 @@ from MetaLex import dicProject
 # ----External Modules------------------------------------------------------
 
 from tesserocr import PyTessBaseAPI
-import codecs
+import codecs, os
 
 # -----Exported Functions-----------------------------------------------------
 
@@ -65,29 +65,33 @@ def imageToText(show=False, save=False, langIn='fra'):
             for i in imagepart :
                 imageconcat +='_'+i 
             imageconcat = imageconcat.split('.')[0]
-            tempname = 'text_ocr'+imageconcat+'_'+str(num)+'.html'
+            tempname = 'text_ocr'+imageconcat+'.html'
             
-            print "\n--> Début de la lecture optique de '"+imagefile+"'\n"
-            textocr = api.GetHOCRText(0)
-            print "\n--> Fin de la lecture optique de '"+imagefile+"'\n"
-            
-            if save:
-                dicProject.createtemp()
-                with codecs.open(tempname, 'w', "utf-8") as wr :
-                    wr.write(textocr)
-                message = "'"+ imagefile +"' is Ocrised to > '"+tempname+"' > Saved in dicTemp folder" 
-                print "--> "+message+"\n"
-                MetaLex.dicLog.manageLog.writelog(message) 
-            elif show :
-                print "\n\n*********************************************************\n\n"
-                print textocr
-                print "\n\n*********************************************************\n\n"
-            else :
-                message = " Warning : imageToText(show=False, save=False) >> precise the action 'show=False or save=False'"
-                MetaLex.dicLog.manageLog.writelog(message) 
+            dicProject.createtemp()
+            if dicProject.inDir(tempname) :
+                print "\n--> Début de la lecture optique de '"+imagefile+"'\n"
+                textocr = api.GetHOCRText(2)
+                print "\n--> Fin de la lecture optique de '"+imagefile+"'\n"
                 
+                if save:
+                    with codecs.open(tempname, 'w', "utf-8") as wr :
+                        wr.write(textocr)
+                    message = "'"+ imagefile +"' is Ocrised to > '"+tempname+"' > Saved in dicTemp folder" 
+                    print "--> "+message+"\n"
+                    MetaLex.dicLog.manageLog.writelog(message) 
+                    MetaLex.resultOcrData[img] = [textocr]
+                elif show :
+                    print "\n\n*********************************************************\n\n"
+                    print textocr
+                    print "\n\n*********************************************************\n\n"
+                else :
+                    message = " Warning : imageToText(show=False, save=False) >> precise the action 'show=False or save=False'"
+                    MetaLex.dicLog.manageLog.writelog(message) 
+            else :
+                print "\n--> Fin de la lecture optique de '"+imagefile+"'\n"
+            
+            os.chdir('..')
             dicProject.treat_ocr_append(tempname)  
-            MetaLex.resultOcrData[img] = [textocr]
             
         num += 1
                 
