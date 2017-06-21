62a93c4b5b3b1760a8326417615b0fac40c952b4
levis0045 <Levis0045@users.noreply.github.com>
2017-06-21 08:57:43 +0200

    create dataArticles function

--- dicXmlised/makeBalise.py
+++ dicXmlised/makeBalise.py
@@ -13,11 +13,13 @@
 # ----Internal Modules------------------------------------------------------
 
 import MetaLex
+from   composeArticle import *
 
 # ----External Modules------------------------------------------------------
 
 import re, sys, codecs
-from bs4 import BeautifulSoup
+from   bs4 import BeautifulSoup
+import os
 
 # -----Exported Functions-----------------------------------------------------
 
@@ -25,20 +27,57 @@ __all__ = ['xmlised', 'dicoHtml']
 
 # -----Global Variables-----------------------------------------------------
 
-allforms = {}
 
+components = {
+                u'xml' : {
+                          u'metadata'       : [u'namespace', u'projectname', u'author', u'date', u'comment', u'contributors'],
+                          u'identification' : [u'entry', u'flexion', u'category', u'gender', u'rection', u'phonetic'], 
+                          u'treatment'      : [u'definition', u'contextualisation', u'figured', u'contrary']
+                        },
+                u'tei' : {
+                          u'metadata'       : [],
+                          u'identification' : [], 
+                          u'treatment'      : []
+                        },
+                u'lmf' : {
+                          u'metadata'       : [],
+                          u'identification' : [], 
+                          u'treatment'      : []
+                        },
+                u'dtd' : [u'ELEMENT', u'ATTRIBUTE', u'PCDATA', u'CDATA', u'REQUIRED', u'IMPLIED'],
+                u'xsd' : []
+             }
 
 # ----------------------------------------------------------
 
-def xmlised(data, typ='xml') :
-    
-    return False
+
+def dataArticles(typ=u'pickle'):
+    MetaLex.dicProject.createtemp()
+    contentdir = os.listdir('.')
+    filepickle = u''
+    filetext   = u''
+    for fil in contentdir :
+        if fil.split('.')[1]   == u'pickle' :
+            filepickle = fil 
+        elif fil.split('.')[1] == u'art'    :
+            filetext = fil
+    if typ :
+        datapickle = MetaLex.dicProject.fileUnpickle(filepickle)
+        return datapickle
+    if typ == u'text' :
+        datatext = MetaLex.dicProject.fileGettext(filetext)
+        return datatext
+
+
+
+def xmlised(typ=u'xml') :
+    data = dataArticles()
+    print data
     
     
     
       
-def buildStructure(data, typ='dtd'):
-    
+def buildStructure(data, typ=u'dtd'):
     return False
 
 
