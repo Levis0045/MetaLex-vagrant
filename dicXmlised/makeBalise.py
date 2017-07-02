5708c7917282ff82fc09d6a19e3c1f4f19aa92f6
levis0045 <Levis0045@users.noreply.github.com>
2017-07-03 00:24:23 +0200

    Update documentation

--- dicXmlised/makeBalise.py
+++ dicXmlised/makeBalise.py
@@ -3,11 +3,15 @@
 
 """
     Implémentation de détection de composants  des articles.
- 
+    
+    Packages:
+        >>> apt-get install python-html5lib
+        >>> apt-get install python-lxml
+        >>> apt-get install python-bs4
     
     Usage:
-    >>> from MetaLex.dicOcrText import *
-    >>> composeArticle()
+        >>> from MetaLex.dicOcrText import *
+        >>> composeArticle()
 """
 
 # ----Internal Modules------------------------------------------------------
@@ -67,7 +71,8 @@ article = []
 
 def getDataArticles(typ=u'pickle'):
     """
-        Get data article from the store data file depending of the type wanted   
+      Get data article from the store data file depending of the type wanted   
+      @return: datapickle or datatext
     """
     MetaLex.dicProject.createtemp()
     contentdir = os.listdir('.')
@@ -89,7 +94,8 @@ def getDataArticles(typ=u'pickle'):
 
 def xmlised(typ=u'xml', save=False) :
     """
-        Create well formed (xml|tei|lmf) file with metadata and content xml 
+      Create well formed (xml|tei|lmf) file with metadata and content xml 
+      @return: metalexXml
     """
     metadata   = xmlMetadata()
     content    = xmlContent()
@@ -97,10 +103,10 @@ def xmlised(typ=u'xml', save=False) :
         if save :
             MetaLex.dicProject.createtemp()
             name = u'MetaLex-'+MetaLex.projectName+u'.xml'
-            metalexxml = balise(metadata+content, u'MetaLexProject', typ=u'')
+            metalexXml = balise(metadata+content, u'MetaLexProject', typ=u'')
             if MetaLex.dicProject.inDir(name) :
                 with codecs.open(name, 'w', 'utf-8') as fle :
-                    fle.write(metalexxml)
+                    fle.write(metalexXml)
                 message = u"'"+name+u"'  is created and contain all dictionary articles formated in xml standard format > Saved in dicTemp folder"
                 MetaLex.dicLog.manageLog.writelog(message)
             else:
@@ -108,14 +114,15 @@ def xmlised(typ=u'xml', save=False) :
                 print message
                 #MetaLex.dicLog.manageLog.writelog(message)
         else :
-            metalexxml = balise(metadata+content, u'MetaLexProject', typ=u'')
-            return metalexxml
+            metalexXml = balise(metadata+content, u'MetaLexProject', typ=u'')
+            return metalexXml
         
     
 
 def xmlMetadata(typ=u'xml'):
     """
-        Create xml metadata file with configuration of the project 
+      Create xml metadata file with configuration of the project 
+      @return:  content (metadata xml)
     """
     MetaLex.dicProject.createtemp()
     if typ == u'xml' :
@@ -139,18 +146,19 @@ def xmlMetadata(typ=u'xml'):
         
 def xmlContent(typ=u'xml'): 
     """
-        Create xml content file (representing articles) with data articles extracting
+      Create xml content file (representing articles) with data articles extracting
+      @return: contentXml
     """
     data    = getDataArticles()
     content = u''
-    result  = u''
+    contentXml  = u''
     if typ == u'xml' :
         for dicart in data :
             for art in dicart.keys() :
                 art = balise(dicart[art], u'mtl:article', typ=u'', art=True)
                 content += art
-        result = balise(content, u'mtl:content', typ=u'', art=True)
-        return result
+        contentXml = balise(content, u'mtl:content', typ=u'', art=True)
+        return contentXml
 
     
       
@@ -160,7 +168,8 @@ def buildStructure(data, typ=u'dtd'):
 
 def dicoHtml(save=False) :
     """
-        Build html editor file of the all articles 
+      Build HTML editor file of the all articles 
+      @return: MetaLexViewerEditor.html
     """
     MetaLex.dicPlugins
     filepath     = sys.path[-1]+u'/MetaLex-template.html'
@@ -186,7 +195,8 @@ def dicoHtml(save=False) :
     
 def htmlInject(template):
     """
-        Create html prettify file all previous data generated 
+      Create HTML prettify file all previous data generated 
+      @return: html (prettify by BeautifulSoup)
     """
     MetaLex.dicProject.createtemp()
     contentxml     = xmlised(typ=u'xml', save=False)
@@ -226,7 +236,8 @@ def htmlInject(template):
         
 def balise(element, markup, typ=u'xml', art=False):
     """
-        Markup data with a specific format type (xml|tei|lmf)
+      Markup data with a specific format type (xml|tei|lmf)
+      @return: balised element
     """
     if type :
         if markup in components[u'xml'][u'identification'] \
@@ -272,7 +283,8 @@ def generateMetadata():
     
 def chevron(el, openchev=True, art=False):
     """
-      Put tag around the data element 
+      Put tag around the data element
+      @return: tagging element 
     """
     idart = generateID()
     if art :
@@ -285,7 +297,8 @@ def chevron(el, openchev=True, art=False):
     
 def generateID():
     """
-        Generate ID of 5 characters with alpha numeric characters 
+      Generate ID of 5 characters with alpha numeric characters 
+      @return: id generated
     """
     idart = sample([u'1',u'2',u'3',u'4',u'5',u'6',u'7',u'8',u'9',u'0',u'a',u'b',u'c',u'd',u'e',u'f',u'g',u'h',u'i',u'j',u'k',u'l',u'm',u'n',u'o',u'p',u'q',u'r',u's',u't',u'v',u'w',u'y',u'z'], k=5)
     return u''.join(idart)
