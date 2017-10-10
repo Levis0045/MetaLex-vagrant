cb1a9e9eb34ef70b17fcad92d0052226f2fd733d
Levis0045 <levismboning@yahoo.fr>
2017-10-10 02:24:59 +0200

    Fix Bug installation

--- setup.sh
+++ setup.sh
@@ -26,30 +26,14 @@
 sudo apt-get install vagrant
 sudo apt-get install virtualbox
 
-#Get MetaLex-vagrant from the official repository
-git clone git@github.com:Levis0045/MetaLex-vagrant.git
-cd MetaLex-vagrant
-
 #Get MetaLex from the official repository
 git clone git@github.com:Levis0045/MetaLex.git
-
-#Install MetaLex's dependencies
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev
-sudo pip install Cython
-sudo apt-get install libtesseract-dev libleptonica-dev libjpeg-dev zlib1g-dev libpng-dev
-sudo apt-get install tesseract-ocr-all
-sudo apt-get install python-html5lib
-sudo apt-get install python-lxml
-sudo apt-get install python-bs4
-sudo pip install pillow
-sudo pip install termcolor
-sudo CPPFLAGS=-I/usr/local/include pip install tesserocr
-    
-#Build vagrant machine with all dependencies
-vagrant up   
-
+ 
 #Update the vagrant box
 vagrant box update 
+
+#Build vagrant machine with all dependencies
+vagrant up  
 
 #Connect to ubuntu xenia development
 vagrant ssh  
